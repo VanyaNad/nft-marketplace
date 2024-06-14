366d5aaa5047ce57cfdf044f3fa250a8bf77e3e1
VanyaNad <vanyand2321@gmail.com>
2024-06-14 15:35:21 +0200

    SignUoPage

--- scripts/script.js
+++ scripts/script.js
@@ -1,9 +1,24 @@
-document.addEventListener('DOMContentLoaded', function () {
-    const burger = document.querySelector('.burger');
-    const headerList = document.querySelector('.HeaderList');
+document.addEventListener("DOMContentLoaded", function() {
+    // Load the header
+    fetch('./htmlTemplates/header.html')
+        .then(response => response.text())
+        .then(data => {
+            document.getElementById('header-container').innerHTML = data;
 
-    burger.addEventListener('click', function () {
-        burger.classList.toggle('open');
-        headerList.classList.toggle('open');
-    });
+            //burger
+            const burger = document.querySelector('.burger');
+            const headerList = document.querySelector('.HeaderList');
+
+            burger.addEventListener('click', function () {
+                burger.classList.toggle('open');
+                headerList.classList.toggle('open');
+            });
+        });
+
+    // Load the footer
+    fetch('./htmlTemplates/footer.html')
+        .then(response => response.text())
+        .then(data => {
+            document.getElementById('footer-container').innerHTML = data;
+        });
 });
